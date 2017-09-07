2e07ffe422a6691e373d1c05f305ba962bb301ec
marcelo <marcelobbff@gmail.com>
2017-09-07 13:17:38 -0300

    Added exception handle to file not found

--- app/controllers/bpmn_controller.rb
+++ app/controllers/bpmn_controller.rb
@@ -1,6 +1,7 @@
 class BpmnController < ApplicationController
   before_action :set_user_tasks, only: [:parser]
   before_action :set_bpmn, only: [:parser]
+  rescue_from  Errno::ENOENT, with: :file_not_found
 
   def modeler
 
@@ -11,7 +12,8 @@ class BpmnController < ApplicationController
   end
 
   def parser
-    doc = Nokogiri::XML(File.open("#{Rails.root}/bpmn/processo_noticia.bpmn"))
+    doc = Nokogiri::XML(File.open("#{Rails.root}/bpmn/processo_noticia.bpmn")) if params[:name].nil?
+    doc = Nokogiri::XML(File.open("#{Rails.root}/bpmn/#{params[:name]}.bpmn")) unless params[:name].nil?
 
     # Get process name
     @process = doc.css('bpmn|participant').first['name']
@@ -31,12 +33,28 @@ class BpmnController < ApplicationController
 
   end
 
+  private
+
   def set_user_tasks
     @userTasks = Array.new
   end
 
   def set_bpmn
     @bpmn = Array.new
+  end
+
+  def file_not_found
+    @files = Dir.entries("#{Rails.root}/bpmn/")
+    respond_to do |format|
+      format.json { render json: "Process #{params[:name]} not found. Existing files:#{@files}" }
+      format.html { render json: "Process #{params[:name]} not found. Existing files:#{@files}", status: :not_found }
+
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def bpmn_params
+    params.require(:bpmn).permit(:name, :process, :userTask)
   end
 
 end
