545b9bbfc95d72b3fd43062321ec51a8798873bd
marcelo <marcelobbff@gmail.com>
2017-09-11 12:44:22 -0300

    Added admin db seed added index view to bpmn Added parser to create business process endpoint

--- app/controllers/bpmn_controller.rb
+++ app/controllers/bpmn_controller.rb
@@ -11,24 +11,28 @@ class BpmnController < ApplicationController
   def viewer
 
   end
-
+  # GET /bpmn/create
+  # Create relational process from xml using nokogiri
   def create
     doc = Nokogiri::XML(File.open("#{Rails.root}/bpmn/processo_noticia.bpmn"))
-    byebug
+
     # Get process name
     puts doc.xpath('//bpmn:participant').first['name']
-    # Get process lanes
-    doc.xpath('//bpmn:lane').each do |lane|
-      puts  " lane: #{lane['name']}"
-    end
+    business_process = BusinessProcess.create(name: doc.xpath('//bpmn:participant').first['name'])
 
-    # Get process lane nodes
-    # And get sequence flows
+
+    # Get process nodes, identify and create. Process lanes, lane nodes and sequence flows.
     process = doc.xpath('//bpmn:process')
     process.children.each do |node|
+
       puts "node: #{node}"
       identify_and_create(node)
 
+    end
+
+    respond_to do |format|
+        format.html { render :index, notice: 'Business process was successfully created.' }
+        format.json { render :show, status: :created }
     end
 
   end
@@ -58,29 +62,35 @@ class BpmnController < ApplicationController
 
   private
 
-  def identify_and_Create(node)
-    return if node.name.eql? 'text'
+  def identify_and_create(node)
+    return if node.name.eql? 'text' # trata node que pega espaçamento e \n
 
-    if node.eql? 'laneSet'
+    if node.name.eql? 'laneSet'
 
       node.xpath('//bpmn:lane').each do |lane|
         puts lane['name'] # Lane.create(name: lane['name'])
-
+        Lane.create(business_process: BusinessProcess.last, name: lane['name'])
         lane.xpath('//bpmn:flowNodeRef').each do |lane_task|
-          puts "Nodo de #{lane['name']} : #{lane_task.text}" #Task.create(xml_id: lane_task.text, lane_id: Lane.last.id )
-        end
 
+          puts "Nodo de #{lane['name']} : #{lane_task.text}"
+          task = Task.create(xml_id: lane_task.text, lane_id: Lane.last.id )
+          render json: task.errors unless task.save
+        end
       end
 
     elsif node.name.eql? 'SequenceFlow'
 
-      # source = Task.find_or_initialize(xml_id: node['sourceRef'])
-      # target = Task.find_or_initialize(xml_id: node['targetRef'])
-      # sequence = SequenceFlow.find_or_initialize(xml_id: node['id'],source: source, target: target)
-      # sequence.save
+      source = Task.first_or_create(xml_id: node['sourceRef'])
+      target = Task.first_or_create(xml_id: node['targetRef'])
+      sequence = SequenceFlow.first_or_create(xml_id: node['id'],source: source, target: target)
+      render json: task.errors unless sequence.save
     else
+
       # Node.find_or_initialize(xml_id: ,category: node.name , content: node['name'])
       # Node.save
+      task = Task.first_or_create(xml_id:node['id'],category: node.name , content: node['name'])
+      render json: task.errors unless task.save
+
     end
 
   end
