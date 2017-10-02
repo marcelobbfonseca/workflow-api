bec3877793a943be6d01e74a2b44b144e9282971
marcelo <marcelobbff@gmail.com>
2017-10-01 22:13:55 -0300

    fixed task and sequence_flow association

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :lane, inverse_of: :tasks, dependent: :destroy
-  has_one :previous_sequence_flow, class_name: 'SequenceFlow', inverse_of: 'source', dependent: :nullify
-  has_one :next_sequence_flow, class_name: 'SequenceFlow', inverse_of: 'target', dependent: :nullify
-
+  has_many :previous_sequence_flows, class_name: 'SequenceFlow', inverse_of: 'source', dependent: :nullify
+  has_many :next_sequence_flows, class_name: 'SequenceFlow', inverse_of: 'target', dependent: :nullify
+  #has_many
   enum category: [:startEvent, :userTask, :task, :exclusiveGateway, :endEvent]
 
   rails_admin do
