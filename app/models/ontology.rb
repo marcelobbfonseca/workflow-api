91235d4137d1f7d50ecf1ada1bd5d6863d7197e4
marcelo <marcelobbff@gmail.com>
2017-10-11 21:56:34 -0300

    added validation to prefix on ontologies

--- app/models/ontology.rb
+++ app/models/ontology.rb
@@ -1,7 +1,7 @@
 class Ontology < ApplicationRecord
   mount_uploader :path_name, OntologyUploader # Tells rails to use this uploader for this model.
   validates :name, presence: true # Make sure the owner's name is present.
-
+  validates_presence_of :prefix
 
 
 end
